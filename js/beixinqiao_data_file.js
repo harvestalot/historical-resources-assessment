b9b0c113760929df8947dc282e890668143ffa48
xinyu <fangjingyu0502@163.com>
2019-08-14 09:08:05 +0800

    更新防止文件缓存不更新

--- js/beixinqiao_data_file.js
+++ js/beixinqiao_data_file.js
@@ -1,33 +1,33 @@
 //北新桥公共静态数据配置文件
-document.write('<script src="./json_data/beixinqiao/introduction_config.js"></script>');
+document.write('<script src="./json_data/beixinqiao/introduction_config.js?V2019.08.14"></script>');
 
 
 //图层数据文件 
-document.write('<script src="./json_data/beixinqiao/beixinqiao_subdistrict.js"></script>');//街道范围边界
-document.write('<script src="./json_data/beixinqiao/beixinqiao_community.js"></script>');//各社区范围边界
-document.write('<script src="./json_data/beixinqiao/control_unit.js"></script>');//管控单元范围边界
-document.write('<script src="./json_data/beixinqiao/current_situation_land.js"></script>');//现状用地各地块数据
-document.write('<script src="./json_data/beixinqiao/road.js"></script>');//街道道路数据
-document.write('<script src="./json_data/beixinqiao/convenience_people_facilities.js"></script>');//便民设施点数据
-document.write('<script src="./json_data/beixinqiao/medical_treatment_facilities.js"></script>');//医疗设施点数据
-document.write('<script src="./json_data/beixinqiao/educational_facilities.js"></script>');//教育设施点数据
-document.write('<script src="./json_data/beixinqiao/recreation_sports_facilities.js"></script>');//文体设施点数据
-document.write('<script src="./json_data/beixinqiao/traffic_facilities.js"></script>');//交通设施点数据
-document.write('<script src="./json_data/beixinqiao/endowment_facilities.js"></script>');//养老设施点数据
-document.write('<script src="./json_data/beixinqiao/street_management_facilities.js"></script>');//街道管理设施点数据
-document.write('<script src="./json_data/beixinqiao/visitors_flow_rate.js"></script>');//街区活力--2016-2018年人流量点数据
+document.write('<script src="./json_data/beixinqiao/beixinqiao_subdistrict.js?V2019.08.14"></script>');//街道范围边界
+document.write('<script src="./json_data/beixinqiao/beixinqiao_community.js?V2019.08.14"></script>');//各社区范围边界
+document.write('<script src="./json_data/beixinqiao/control_unit.js?V2019.08.14"></script>');//管控单元范围边界
+document.write('<script src="./json_data/beixinqiao/current_situation_land.js?V2019.08.14"></script>');//现状用地各地块数据
+document.write('<script src="./json_data/beixinqiao/road.js?V2019.08.14"></script>');//街道道路数据
+document.write('<script src="./json_data/beixinqiao/convenience_people_facilities.js?V2019.08.14"></script>');//便民设施点数据
+document.write('<script src="./json_data/beixinqiao/medical_treatment_facilities.js?V2019.08.14"></script>');//医疗设施点数据
+document.write('<script src="./json_data/beixinqiao/educational_facilities.js?V2019.08.14"></script>');//教育设施点数据
+document.write('<script src="./json_data/beixinqiao/recreation_sports_facilities.js?V2019.08.14"></script>');//文体设施点数据
+document.write('<script src="./json_data/beixinqiao/traffic_facilities.js?V2019.08.14"></script>');//交通设施点数据
+document.write('<script src="./json_data/beixinqiao/endowment_facilities.js?V2019.08.14"></script>');//养老设施点数据
+document.write('<script src="./json_data/beixinqiao/street_management_facilities.js?V2019.08.14"></script>');//街道管理设施点数据
+document.write('<script src="./json_data/beixinqiao/visitors_flow_rate.js?V2019.08.14"></script>');//街区活力--2016-2018年人流量点数据
 
 
 //图表数据可视化文件
-document.write('<script src="./js/beixinqiao/population.js"></script>');//人口信息
-document.write('<script src="./js/beixinqiao/public_service_overview.js"></script>');//公共服务设施（概览）
-document.write('<script src="./js/beixinqiao/public_service_convenience_people.js"></script>');//公共服务设施（便民）
-document.write('<script src="./js/beixinqiao/public_service_medical_treatment.js"></script>');//公共服务设施（医疗）
-document.write('<script src="./js/beixinqiao/public_service_education.js"></script>');//公共服务设施（教育）
-document.write('<script src="./js/beixinqiao/public_service_recreation_sports.js"></script>');//公共服务设施（文体）
-document.write('<script src="./js/beixinqiao/public_service_traffic_facilities.js"></script>');//公共服务设施（交通）
-document.write('<script src="./js/beixinqiao/public_service_provide.js"></script>');//公共服务设施（养老）
-document.write('<script src="./js/beixinqiao/public_service_street_management.js"></script>');//公共服务设施（街道管理）
-document.write('<script src="./js/beixinqiao/industrial_structure_concentration.js"></script>');//产业结构（产业聚集度）
-document.write('<script src="./js/beixinqiao/industrial_structure_development.js"></script>');//产业结构（产业发展）
-document.write('<script src="./js/beixinqiao/block_energy_visitors_flow_rate.js"></script>');//街区活力--人流量监测
+document.write('<script src="./js/beixinqiao/population.js?V2019.08.14"></script>');//人口信息
+document.write('<script src="./js/beixinqiao/public_service_overview.js?V2019.08.14"></script>');//公共服务设施（概览）
+document.write('<script src="./js/beixinqiao/public_service_convenience_people.js?V2019.08.14"></script>');//公共服务设施（便民）
+document.write('<script src="./js/beixinqiao/public_service_medical_treatment.js?V2019.08.14"></script>');//公共服务设施（医疗）
+document.write('<script src="./js/beixinqiao/public_service_education.js?V2019.08.14"></script>');//公共服务设施（教育）
+document.write('<script src="./js/beixinqiao/public_service_recreation_sports.js?V2019.08.14"></script>');//公共服务设施（文体）
+document.write('<script src="./js/beixinqiao/public_service_traffic_facilities.js?V2019.08.14"></script>');//公共服务设施（交通）
+document.write('<script src="./js/beixinqiao/public_service_provide.js?V2019.08.14"></script>');//公共服务设施（养老）
+document.write('<script src="./js/beixinqiao/public_service_street_management.js?V2019.08.14"></script>');//公共服务设施（街道管理）
+document.write('<script src="./js/beixinqiao/industrial_structure_concentration.js?V2019.08.14"></script>');//产业结构（产业聚集度）
+document.write('<script src="./js/beixinqiao/industrial_structure_development.js?V2019.08.14"></script>');//产业结构（产业发展）
+document.write('<script src="./js/beixinqiao/block_energy_visitors_flow_rate.js?V2019.08.14"></script>');//街区活力--人流量监测
