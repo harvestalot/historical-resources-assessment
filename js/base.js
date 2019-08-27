d2c4acdaed9b2e2b161c24a598fd633363ffdf5b
xinyu <fangjingyu0502@163.com>
2019-08-27 17:53:44 +0800

    浏览器兼容提示页

--- js/base.js
+++ js/base.js
@@ -1,3 +1,35 @@
+
+// (function(window) {
+//     var userAgent = navigator.userAgent; //取得浏览器的userAgent字符串  
+//     var isIE = userAgent.indexOf("compatible") > -1 && userAgent.indexOf("MSIE") > -1; //判断是否IE<11浏览器  
+//     var isEdge = userAgent.indexOf("Edge") > -1 && !isIE; //判断是否IE的Edge浏览器  
+//     var isIE11 = userAgent.indexOf('Trident') > -1 && userAgent.indexOf("rv:11.0") > -1;
+//     if(isIE) {
+//         window.location.href = "https://www.so.com";
+//         var reIE = new RegExp("MSIE (\\d+\\.\\d+);");
+//         reIE.test(userAgent);
+//         var fIEVersion = parseFloat(RegExp["$1"]);
+//         alert(fIEVersion);
+//         if(fIEVersion == 7) {
+//             return 7;
+//         } else if(fIEVersion == 8) {
+//             window.location.href = "https://www.so.com";
+//             return 8;
+//         } else if(fIEVersion == 9) {
+//             return 9;
+//         } else if(fIEVersion == 10) {
+//             return 10;
+//         } else {
+//             return 6;//IE版本<=7
+//         }   
+//     } else if(isEdge) {
+//         return 'edge';//edge
+//     } else if(isIE11) {
+//         return 11; //IE11  
+//     }else{
+//         return -1;//不是ie浏览器
+//     }
+// })(window);
 //外部css、js库
 document.write('<link rel="stylesheet" type="text/css" href="./lib/animate.css">');
 document.write('<link rel="stylesheet" href="https://cache.amap.com/lbs/static/main1119.css"/>');
